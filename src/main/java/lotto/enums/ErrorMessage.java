0bf5118d0b9ea2a7bcd48abb0a0cd905ea556a02
wontop02 <wjy0502kr@naver.com>
2025-10-31 00:04:33 +0900

    feat: 문자열에 숫자를 제외한 문자가 존재하는지 검증하는 기능 구현

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -1,7 +1,8 @@
 package lotto.enums;
 
 public enum ErrorMessage {
-    BLANK_INPUT("[ERROR] 빈 문자열이 입력되었습니다. 다시 한번 입력해 주세요.");
+    BLANK_INPUT("[ERROR] 빈 문자열이 입력되었습니다. 다시 입력해 주세요."),
+    NOT_ONLY_DIGIT("[ERROR] 숫자를 제외한 문자가 포함되어 있습니다. 다시 입력해 주세요.");
 
     private final String message;
 
--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -3,14 +3,22 @@ package lotto.validator;
 import lotto.enums.ErrorMessage;
 
 public class InputValidator {
+    private static final String ONLY_DIGIT_REGEX = "^[0-9]+$";
     private static void validateNotBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.BLANK_INPUT.getMessage());
         }
     }
 
+    private static void validateOnlyDigit(String input) {
+        if (!input.matches(ONLY_DIGIT_REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_ONLY_DIGIT.getMessage());
+        }
+    }
+
     public static void validatePurchaseAmount(String input) {
         validateNotBlank(input);
+        validateOnlyDigit(input);
     }
 
     public static void validateWinningNumbers(String input) {
@@ -19,5 +27,6 @@ public class InputValidator {
 
     public static void validateBonusNumber(String input) {
         validateNotBlank(input);
+        validateOnlyDigit(input);
     }
 }
