89d6523423290c030362fdfaa04e387c0e388b68
wontop02 <wjy0502kr@naver.com>
2025-10-31 00:25:57 +0900

    test: 당첨 번호에 숫자와 쉼표를 제외한 문자가 포함되어 있을 경우 예외 발생 테스트 추가

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -12,6 +13,8 @@ class InputValidatorTest {
             "[ERROR] 빈 문자열이 입력되었습니다. 다시 입력해 주세요.";
     private static final String NOT_ONLY_DIGIT =
             "[ERROR] 숫자를 제외한 문자가 포함되어 있습니다. 다시 입력해 주세요.";
+    private static final String NOT_DIGITS_AND_COMMA_ONLY =
+            "[ERROR] 숫자와 쉼표(,)를 제외한 문자가 포함되어 있습니다. 다시 입력해 주세요";
 
     private static Stream<Runnable> validatorsForBlankInput() {
         return Stream.of(
@@ -44,5 +47,13 @@ class InputValidatorTest {
         assertThatThrownBy(validatorCall::run)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(NOT_ONLY_DIGIT);
+    }
+
+    @DisplayName("숫자와 쉼표를 제외한 문자가 존재하면 예외가 발생한다.")
+    @Test
+    void 숫자와_쉼표를_제외한_문자가_존재하면_예외가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1, 2, 3, 4, 5, 6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_DIGITS_AND_COMMA_ONLY);
     }
 }
