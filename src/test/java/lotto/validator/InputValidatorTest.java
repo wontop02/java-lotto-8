d583b7cd449760bc8ed11ca3fc561ed83d038478
wontop02 <wjy0502kr@naver.com>
2025-10-31 00:03:52 +0900

    test: 숫자를 제외한 문자가 존재할 경우 예외 발생 테스트 추가

--- src/test/java/lotto/validator/InputValidatorTest.java
+++ src/test/java/lotto/validator/InputValidatorTest.java
@@ -7,14 +7,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class InputValidatorTest {
+class InputValidatorTest {
     private static final String BLANK_INPUT =
-            "[ERROR] 빈 문자열이 입력되었습니다. 다시 한번 입력해 주세요.";
+            "[ERROR] 빈 문자열이 입력되었습니다. 다시 입력해 주세요.";
+    private static final String NOT_ONLY_DIGIT =
+            "[ERROR] 숫자를 제외한 문자가 포함되어 있습니다. 다시 입력해 주세요.";
+
     private static Stream<Runnable> validatorsForBlankInput() {
         return Stream.of(
                 () -> InputValidator.validatePurchaseAmount(" "),
                 () -> InputValidator.validateWinningNumbers(" "),
                 () -> InputValidator.validateBonusNumber(" ")
+        );
+    }
+
+    private static Stream<Runnable> validatorsForNotOnlyDigit() {
+        return Stream.of(
+                () -> InputValidator.validatePurchaseAmount("-100"),
+                () -> InputValidator.validateBonusNumber("5.2")
         );
     }
 
@@ -25,5 +35,14 @@ public class InputValidatorTest {
         assertThatThrownBy(validatorCall::run)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(BLANK_INPUT);
+    }
+
+    @DisplayName("숫자를 제외한 문자가 존재하면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("validatorsForNotOnlyDigit")
+    void 숫자를_제외한_문자가_존재하면_예외가_발생한다(Runnable validatorCall) {
+        assertThatThrownBy(validatorCall::run)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_ONLY_DIGIT);
     }
 }
