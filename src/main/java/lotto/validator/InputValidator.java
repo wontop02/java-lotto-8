2334411d1a375156b3109be6ded340a15c3ba1c9
wontop02 <wjy0502kr@naver.com>
2025-10-31 05:25:19 +0900

    feat: 입력된 숫자가 int 범위를 벗어났는지 검증하는 기능 구현

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,5 +1,8 @@
 package lotto.validator;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
 import lotto.enums.ErrorMessage;
 
 public class InputValidator {
@@ -24,18 +27,31 @@ public class InputValidator {
         }
     }
 
+    private static void validateWithinIntRange(String input, ErrorMessage errorMessage) {
+        BigInteger value = new BigInteger(input);
+        if (value.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) < 0
+                || value.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) > 0) {
+            throw new IllegalArgumentException(errorMessage.getMessage());
+        }
+    }
+
     public static void validatePurchaseAmount(String input) {
         validateNotBlank(input);
         validateOnlyDigit(input);
+        validateWithinIntRange(input, ErrorMessage.INVALID_PURCHASE_AMOUNT_RANGE);
     }
 
     public static void validateWinningNumbers(String input) {
         validateNotBlank(input);
         validateDigitsAndCommaOnly(input);
+        List<String> numbers = Arrays.asList(input.split(",", -1));
+        numbers.forEach(InputValidator::validateNotBlank);
+        numbers.forEach(number -> validateWithinIntRange(number, ErrorMessage.INVALID_WINNING_NUMBER_RANGE));
     }
 
     public static void validateBonusNumber(String input) {
         validateNotBlank(input);
         validateOnlyDigit(input);
+        validateWithinIntRange(input, ErrorMessage.INVALID_BONUS_NUMBER_RANGE);
     }
 }
