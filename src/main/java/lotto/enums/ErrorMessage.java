fdceb5d278f25efe63765c2ecc27856453ec5766
wontop02 <wjy0502kr@naver.com>
2025-10-31 00:26:49 +0900

    feat: 당첨 번호에 숫자와 쉼표를 제외한 문자가 포함되어 있는지 검증하는 기능 구현

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -2,7 +2,8 @@ package lotto.enums;
 
 public enum ErrorMessage {
     BLANK_INPUT("[ERROR] 빈 문자열이 입력되었습니다. 다시 입력해 주세요."),
-    NOT_ONLY_DIGIT("[ERROR] 숫자를 제외한 문자가 포함되어 있습니다. 다시 입력해 주세요.");
+    NOT_ONLY_DIGIT("[ERROR] 숫자를 제외한 문자가 포함되어 있습니다. 다시 입력해 주세요."),
+    NOT_DIGITS_AND_COMMA_ONLY("[ERROR] 숫자와 쉼표(,)를 제외한 문자가 포함되어 있습니다. 다시 입력해 주세요");
 
     private final String message;
 
--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -4,6 +4,8 @@ import lotto.enums.ErrorMessage;
 
 public class InputValidator {
     private static final String ONLY_DIGIT_REGEX = "^[0-9]+$";
+    private static final String DIGITS_AND_COMMA_ONLY_REGEX = "^[0-9,]+$";
+
     private static void validateNotBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.BLANK_INPUT.getMessage());
@@ -16,6 +18,12 @@ public class InputValidator {
         }
     }
 
+    private static void validateDigitsAndCommaOnly(String input) {
+        if (!input.matches(DIGITS_AND_COMMA_ONLY_REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_DIGITS_AND_COMMA_ONLY.getMessage());
+        }
+    }
+
     public static void validatePurchaseAmount(String input) {
         validateNotBlank(input);
         validateOnlyDigit(input);
@@ -23,6 +31,7 @@ public class InputValidator {
 
     public static void validateWinningNumbers(String input) {
         validateNotBlank(input);
+        validateDigitsAndCommaOnly(input);
     }
 
     public static void validateBonusNumber(String input) {
