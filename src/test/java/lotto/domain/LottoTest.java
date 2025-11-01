eb2431f8b2e4db6ffeb967d26cfd0aa2152b40fa
wontop02 <wjy0502kr@naver.com>
2025-11-02 00:43:32 +0900

    test: 로또 번호가 최소, 최대 범위를 벗어났을 경우 예외 발생 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,15 +1,26 @@
 package lotto.domain;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static lotto.enums.ErrorMessage.DUPLICATED_LOTTO_NUMBER;
 import static lotto.enums.ErrorMessage.INVALID_LOTTO_NUMBER_SIZE;
+import static lotto.enums.ErrorMessage.INVALID_NUMBER_RANGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+    private static Stream<Runnable> invalidNumberRangeInput() {
+        return Stream.of(
+                () -> new Lotto(List.of(0, 1, 2, 3, 4, 5)),
+                () -> new Lotto(List.of(46, 1, 2, 3, 4, 5))
+        );
+    }
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -23,5 +34,14 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(DUPLICATED_LOTTO_NUMBER.getMessage());
+    }
+
+    @DisplayName("로또 번호가 최소 최대 범위를 벗어나면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("invalidNumberRangeInput")
+    void 로또_번호가_최소_최대_범위를_벗어나면_예외가_발생한다(Runnable lottoConstructor) {
+        assertThatThrownBy(lottoConstructor::run)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_NUMBER_RANGE.getMessage());
     }
 }
