a737b8176762aeb5d6c0f2dc71ccd4c1303bc7c5
wontop02 <wjy0502kr@naver.com>
2025-11-02 03:45:48 +0900

    test: 로또 당첨 등수를 판단하는 기능 테스트 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -2,8 +2,10 @@ package lotto.domain;
 
 import static lotto.enums.ErrorMessage.DUPLICATED_WINNING_AND_BONUS_NUMBER;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
+import lotto.enums.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,8 +15,21 @@ class WinningLottoTest {
     void 보너스_번호가_당첨_번호와_중복되면_예외가_발생한다() {
         Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         int bonusNumber = 1;
+
         assertThatThrownBy(() -> new WinningLotto(winningLotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(DUPLICATED_WINNING_AND_BONUS_NUMBER.getMessage());
+    }
+
+    @DisplayName("로또 당첨 등수를 판단한다.")
+    @Test
+    void 로또_당첨_등수를_판단한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 45));
+        int bonusNumber = 6;
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+        Rank rank = winningLotto.match(lotto);
+        assertThat(rank).isEqualTo(Rank.SECOND_PLACE);
     }
 }
