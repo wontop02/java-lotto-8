d13853a3f45a96e7ff62f6816a5744ec02c528e1
wontop02 <wjy0502kr@naver.com>
2025-11-01 17:58:23 +0900

    test: 구입 금액에 해당하는 만큼 로또 발행 기능 테스트 추가

---
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoMachineTest {
+    @DisplayName("구입 금액에 해당하는 만큼 로또를 발행한다.")
+    @Test
+    void 구입_금액에_해당하는_만큼_로또를_발행한다() {
+        PurchaseAmount purchaseAmount = new PurchaseAmount("10000");
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+        LottoMachine lottoMachine = new LottoMachine(purchaseAmount, lottoNumberGenerator);
+        
+        List<Lotto> lottos = lottoMachine.issue();
+
+        assertEquals(10, lottos.size());
+    }
+}
