4f6c3ea4fee14fd9ddea38d34d9ec3bd6ceb221e
wontop02 <wjy0502kr@naver.com>
2025-11-02 22:11:09 +0900

    refactor: 클래스명 오타 수정

--- src/main/java/lotto/constant/PrintMessageConstant.java
+++ src/main/java/lotto/constant/PrintMessageConstant.java
@@ -1,16 +1,16 @@
 package lotto.constant;
 
-public final class printMessageConstant {
+public final class PrintMessageConstant {
     public static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    public static final String REQUEST_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    public static final String REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String REQUEST_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    public static final String REQUEST_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 
     public static final String ERROR_PREFIX = "[ERROR] ";
     public static final String ERROR_SUFFIX = " 다시 입력해 주세요.";
 
-    public static final String PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
+    public static final String PURCHASE_COUNT_MESSAGE = "\n%d개를 구매했습니다.\n";
 
-    public static final String RESULT_TITLE = "당첨 통계\n---";
+    public static final String RESULT_TITLE = "\n당첨 통계\n---";
     public static final String RESULT_FORMAT = "%d개 일치%s (%,d원) - %d개\n";
     public static final String BONUS_MESSAGE = ", 보너스 볼 일치";
     public static final String PROFIT_FORMAT = "총 수익률은 %.1f%%입니다.\n";
