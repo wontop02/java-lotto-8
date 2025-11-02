37d1abbcadfd36a9e59b16f7fa5873cbdc072514
wontop02 <wjy0502kr@naver.com>
2025-11-02 20:20:49 +0900

    refactor: LottoResult 테스트 케이스 변경

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -11,11 +11,17 @@ import org.junit.jupiter.api.Test;
 
 class LottoResultTest {
     private final List<Lotto> lottos = List.of(
-            new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-            new Lotto(List.of(7, 8, 9, 10, 11, 12))
+            new Lotto(List.of(8, 21, 23, 41, 42, 43)),
+            new Lotto(List.of(3, 5, 11, 16, 32, 38)),
+            new Lotto(List.of(7, 11, 16, 35, 36, 44)),
+            new Lotto(List.of(1, 8, 11, 31, 41, 42)),
+            new Lotto(List.of(13, 14, 16, 38, 42, 45)),
+            new Lotto(List.of(7, 11, 30, 40, 42, 43)),
+            new Lotto(List.of(2, 13, 22, 32, 38, 45)),
+            new Lotto(List.of(1, 3, 5, 14, 22, 45))
     );
     private final WinningLotto winningLotto = new WinningLotto(
-            new Lotto(List.of(1, 2, 3, 4, 5, 6)), 45
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7
     );
     private final LottoResult lottoResult = new LottoResult(lottos, winningLotto);
 
@@ -24,21 +30,20 @@ class LottoResultTest {
     void 모든_로또의_당첨_결과를_판단한다() {
         Map<Rank, Integer> rankCount = lottoResult.getRankCount();
 
-        assertThat(rankCount.get(Rank.FIRST_PLACE)).isEqualTo(1);
-        assertThat(rankCount.get(Rank.NOTHING)).isEqualTo(1);
+        assertThat(rankCount.get(Rank.FIFTH_PLACE)).isEqualTo(1);
     }
 
     @DisplayName("모든 로또의 총 당첨 금액을 계산한다.")
     @Test
     void 모든_로또의_총_당첨_금액을_계산한다() {
         long totalPrize = lottoResult.calculateTotalPrize();
-        assertEquals(2_000_000_000L, totalPrize);
+        assertEquals(5_000L, totalPrize);
     }
 
     @DisplayName("수익률을 둘째 자리에서 반올림해 계산한다.")
     @Test
     void 수익률을_둘째_자리에서_반올림해_계산한다() {
-        double profitRate = lottoResult.calculateProfitRate(2000L);
-        assertEquals(100_000_000.0, profitRate);
+        double profitRate = lottoResult.calculateProfitRate(8000L);
+        assertEquals(62.5, profitRate);
     }
 }
