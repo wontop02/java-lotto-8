4d3e2e4d900b5024b5cbccfffbd223162bc8a127
wontop02 <wjy0502kr@naver.com>
2025-11-02 23:32:57 +0900

    feat: List 타입 변환을 담당하는 LottoParser 생성

---
+++ src/main/java/lotto/util/LottoParser.java
@@ -0,0 +1,21 @@
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
+
+public class LottoParser {
+    private LottoParser() {}
+
+    public static List<List<Integer>> toNumberLists(List<Lotto> lottos) {
+        return lottos.stream()
+                .map(Lotto::getNumbers)
+                .toList();
+    }
+
+    public static List<Integer> toNumbers(String input) {
+        return Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();
+    }
+}
