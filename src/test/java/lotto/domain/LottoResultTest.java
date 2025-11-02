d898fa871d8a85eb928f4ca95d666517390eb8d7
wontop02 <wjy0502kr@naver.com>
2025-11-02 19:45:44 +0900

    test: 모든 로또의 총 당첨 금액을 계산하는 기능 테스트 추가

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import java.util.Map;
@@ -9,19 +10,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoResultTest {
-    @DisplayName("모든 로또의 당첨 결과를 판단한다")
+    private final List<Lotto> lottos = List.of(
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+            new Lotto(List.of(7, 8, 9, 10, 11, 12))
+    );
+    private final WinningLotto winningLotto = new WinningLotto(
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)), 45
+    );
+    private final LottoResult lottoResult = new LottoResult(lottos, winningLotto);
+
+    @DisplayName("모든 로또의 당첨 결과를 판단한다.")
     @Test
     void 모든_로또의_당첨_결과를_판단한다() {
-        Lotto firstLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        Lotto nothingLotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
-        List<Lotto> lottos = List.of(firstLotto, nothingLotto);
-        Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, 45);
-
-        LottoResult lottoResult = new LottoResult(lottos, winningLotto);
         Map<Rank, Integer> rankCount = lottoResult.getRankCount();
 
         assertThat(rankCount.get(Rank.FIRST_PLACE)).isEqualTo(1);
         assertThat(rankCount.get(Rank.NOTHING)).isEqualTo(1);
+    }
+
+    @DisplayName("모든 로또의 총 당첨 금액을 계산한다.")
+    @Test
+    void 모든_로또의_총_당첨_금액을_계산한다() {
+        long totalPrize = lottoResult.calculateTotalPrize();
+        assertEquals(2_000_000_000L, totalPrize);
     }
 }
